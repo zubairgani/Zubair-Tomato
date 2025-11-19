853b4126a0286a59d28587fe383be437ca7f07d0
zubairgani <zubairgani003@gmail.com>
2025-11-19 20:00:40 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //config variables
 const currency = "inr";
 const deliveryCharge = 50;
-const frontend_URL = "http://localhost:5173";
+const frontend_URL = "https://tomato-frontend-pcup.onrender.com/";
 
 // Placing User Order for Frontend using stripe
 const placeOrder = async (req, res) => {
